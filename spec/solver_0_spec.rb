457de7f8cb6b1aa631cd18d1e37477a48dda5966
Steve Gagné <steve@grasseh.com>
2019-11-12 12:21:00 -0500

    Add input wrapper and wrapped tests in arrays

--- spec/solver_0_spec.rb
+++ spec/solver_0_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../solvers/solver_0.rb'
+require_relative './spec_helper.rb'
+require 'pry'
+
+RSpec.configure do |c|
+  c.include Helpers
+end
 
 RSpec.describe Solvers::Solver0 do
   before(:each) do
@@ -6,50 +12,31 @@ RSpec.describe Solvers::Solver0 do
   end
 
   context 'Problem A' do
-    it 'returns the entrance on the wrapped parens' do
-      expect(@solver.solve_a(["(())\n"])).to eq(0)
-    end
-
-    it 'returns the entrance on the pairs of parens' do
-      expect(@solver.solve_a(["()()\n"])).to eq(0)
-    end
-
-    it 'gets up to the third floor if it just goes up' do
-      expect(@solver.solve_a(["(((\n"])).to eq(3)
-    end
-
-    it 'can go up and down to the third floor' do
-      expect(@solver.solve_a(["(()(()(\n"])).to eq(3)
-    end
-
-    it 'can go down first and then straight up to the third floor' do
-      expect(@solver.solve_a(["))(((((\n"])).to eq(3)
-    end
-
-    it 'can go up and then reach the basement' do
-      expect(@solver.solve_a(["())\n"])).to eq(-1)
-    end
-
-    it 'can go down and up in the basement' do
-      expect(@solver.solve_a(["))(\n"])).to eq(-1)
-    end
-
-    it 'can go straight to B3F' do
-      expect(@solver.solve_a([")))\n"])).to eq(-3)
-    end
-
-    it 'can go to B3F by messing around' do
-      expect(@solver.solve_a([")())())\n"])).to eq(-3)
+    [
+      ["(())", 0, 'returns the entrance on the wrapped parens'],
+      ["()()", 0, 'returns the entrance on the pairs of parens'],
+      ["(((", 3, 'gets up to the third floor if it just goes up'],
+      ["(()(()(", 3, 'can go up and down to the third floor'],
+      ["))(((((", 3, 'can go down first and then straight up to the third floor'],
+      ["())", -1, 'can go up and then reach the basement'],
+      ["))(", -1, 'can go down and up in the basement'],
+      [")))", -3, 'can go straight to B3F'],
+      [")())())", -3, 'can go to B3F by messing around'],
+    ].each do |(input, expected, description)|
+      it description do
+        expect(@solver.solve_a(wrap_line_input(input))).to eq(expected)
+      end
     end
   end
 
   context 'Problem B' do
-    it 'returns the first move if he goes straight to the basement' do
-      expect(@solver.solve_b([")\n"])).to eq(1)
-    end
-
-    it 'returns the fifth move if he messes around' do
-      expect(@solver.solve_b(["()())\n"])).to eq(5)
+    [
+      [")", 1, 'returns the first move if he goes straight to the basement'],
+      ["()())", 5, 'returns the fifth move if he messes around'],
+    ].each do |(input, expected, description)|
+      it description do
+        expect(@solver.solve_b(wrap_line_input(input))).to eq(expected)
+      end
     end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+module Helpers
+  def wrap_line_input(input)
+    ["#{input}\n"]
+  end
+end
