427be52750027e5e1dd11bf1e19dfc5a96db2efb
Steve Gagné <steve@grasseh.com>
2019-12-03 22:37:38 -0500

    Day 1: The Tyranny of the Rocket Equation

--- solvers/solver_1.rb
+++ solvers/solver_1.rb
@@ -1,27 +1,29 @@
-class Solvers::Solver1
-  def solve_a(input)
-    input = input.map(&:chomp).map(&:to_i)
+module Solvers
+  class Solver1
+    def solve_a(input)
+      input = input.map(&:chomp).map(&:to_i)
 
-    input.reduce(0){ |sum, line| sum + calculate_mass(line) }
-  end
+      input.reduce(0){ |sum, line| sum + calculate_mass(line) }
+    end
 
-  def solve_b(input)
-    input = input.map(&:chomp).map(&:to_i)
+    def solve_b(input)
+      input = input.map(&:chomp).map(&:to_i)
 
-    input.reduce(0){ |sum, line| sum + calculate_fuel(line) }
-  end
+      input.reduce(0){ |sum, line| sum + calculate_fuel(line) }
+    end
 
-  private
+    private
 
-  def calculate_mass(line)
-    (line / 3) - 2
-  end
+    def calculate_mass(line)
+      (line / 3) - 2
+    end
 
-  def calculate_fuel(line)
-    required = calculate_mass(line)
+    def calculate_fuel(line)
+      required = calculate_mass(line)
 
-    return 0 if required <= 0
+      return 0 if required <= 0
 
-    required + calculate_fuel(required)
+      required + calculate_fuel(required)
+    end
   end
 end
