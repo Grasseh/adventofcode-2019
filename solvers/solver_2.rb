4a836203e7fa2a1b12cf12dcad12b3e0f9169b84
Steve Gagné <steve@grasseh.com>
2019-12-03 22:38:22 -0500

    Day 2: 1202 Program Alarm

--- solvers/solver_2.rb
+++ solvers/solver_2.rb
@@ -1,71 +1,73 @@
-class Solvers::Solver2
-  def solve_a(input)
-    input = input.first.chomp.split(',').map(&:to_i)
-    input[1] = 12
-    input[2] = 2
+module Solvers
+  class Solver2
+    def solve_a(input)
+      input = input.first.chomp.split(',').map(&:to_i)
+      input[1] = 12
+      input[2] = 2
 
-    compute(input).first
-  end
+      compute(input).first
+    end
 
-  def solve_b(input)
-    input = input.first.chomp.split(',').map(&:to_i)
+    def solve_b(input)
+      input = input.first.chomp.split(',').map(&:to_i)
 
-    noun = 0
-    verb = 0
-    val = 0
+      noun = 0
+      verb = 0
+      val = 0
 
-    (0..99).each do |v|
-      (0..99).each do |n|
-        copy = input.dup
-        noun = n
-        verb = v
+      (0..99).each do |v|
+        (0..99).each do |n|
+          copy = input.dup
+          noun = n
+          verb = v
 
-        copy[1] = noun
-        copy[2] = verb
+          copy[1] = noun
+          copy[2] = verb
 
-        val = compute(copy).first
+          val = compute(copy).first
+
+          break if val == 19_690_720
+        end
 
         break if val == 19_690_720
       end
 
-      break if val == 19_690_720
+      100 * noun + verb
     end
 
-    100 * noun + verb
-  end
+    def compute(program)
+      i = 0
 
-  def compute(program)
-    i = 0
+      operations = {
+        1 => { op: method(:op_1), inc: 4 },
+        2 => { op: method(:op_2), inc: 4 }
+      }
 
-    operations = {
-      1 => { op: method(:op_1), inc: 4 },
-      2 => { op: method(:op_2), inc: 4 }
-    }
+      loop do
+        operation = program[i]
 
-    loop do
-      operation = program[i]
+        break unless operations.key?(operation)
 
-      break unless operations.key?(operation)
+        program = operations.dig(operation, :op).call(program, i)
 
-      program = operations.dig(operation, :op).call(program, i)
+        i += operations.dig(operation, :inc)
+      end
 
-      i += operations.dig(operation, :inc)
+      program
     end
 
-    program
-  end
+    def op_1(program, position)
+      program[program[position + 3]] = program[program[position + 1]] +
+        program[program[position + 2]]
 
-  def op_1(program, position)
-    program[program[position + 3]] = program[program[position + 1]] +
-      program[program[position + 2]]
+      program
+    end
 
-    program
-  end
+    def op_2(program, position)
+      program[program[position + 3]] = program[program[position + 1]] *
+        program[program[position + 2]]
 
-  def op_2(program, position)
-    program[program[position + 3]] = program[program[position + 1]] *
-      program[program[position + 2]]
-
-    program
+      program
+    end
   end
 end
