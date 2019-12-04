0a517075f5d09a51425cf47b4a31cc3ad20d3cae
Steve Gagné <steve@grasseh.com>
2019-12-03 22:37:00 -0500

    Day 1: Not Quite Lisp

--- solvers/solver_0.rb
+++ solvers/solver_0.rb
@@ -1,16 +1,18 @@
-class Solvers::Solver0
-  def solve_a(input)
-    input = input.first.chomp
-    input.count('(') - input.count(')')
-  end
-
-  def solve_b(input)
-    input = input.first.chomp
-
-    (input.length - 1).times do
-      return i + 1 if input[0..i].count(')') > input[0..i].count('(')
+module Solvers
+  class Solver0
+    def solve_a(input)
+      input = input.first.chomp
+      input.count('(') - input.count(')')
     end
 
-    -1
+    def solve_b(input)
+      input = input.first.chomp
+
+      (input.length - 1).times do
+        return i + 1 if input[0..i].count(')') > input[0..i].count('(')
+      end
+
+      -1
+    end
   end
 end
